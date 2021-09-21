b0dec0be41e4c45a5f728e5298b74d895353b6b6
LiangJianyi <a.124p@live.com>
2021-09-21 18:55:22 +0800

    Changesets=03

--- CombineGPGEncrypText/main.swift
+++ CombineGPGEncrypText/main.swift
@@ -1,14 +1,12 @@
 import Foundation
 
-private let outputDirPath = "/Users/\(NSUserName())/Desktop/tmp2/"
-private let arguments = CommandLine.arguments
+public let arguments = CommandLine.arguments
 
-private var readDirectoryPath: String?
-private var writeFilePath: String?
-private var printLogSwitch: String?
-private var isPrintLog: Bool = false
-private var compareWithOriginalText: Bool = false
-private var cnToEn: Bool = false
+public var readDirPath: String?
+public var writeFilePath: String?
+public var printLogSwitch: String?
+public var isPrintLog: Bool = false
+public var cnToEn: Bool = false
 
 enum CombineGPGEncrypTextError: Error {
     case missingArgument(argumentName: String)
@@ -20,10 +18,10 @@ enum CombineGPGEncrypTextError: Error {
     case writeFileURLIsNull
 }
 
-private func sortContentOfDirectory(dirPath: String) -> [String] {
-    var filesArray = try! FileManager.default.contentsOfDirectory(atPath: dirPath)
+private func sortContentOfDirectory() -> [String] {
+    var filesArray = try! FileManager.default.contentsOfDirectory(atPath: readDirPath!)
     filesArray = filesArray.map{ filename in
-        var res = URL(fileURLWithPath: dirPath).appendingPathComponent(filename).absoluteString
+        var res = URL(fileURLWithPath: readDirPath!).appendingPathComponent(filename).absoluteString
         res.removeFirst(7)
         return res
     }
@@ -35,36 +33,44 @@ private func sortContentOfDirectory(dirPath: String) -> [String] {
     return filesArray
 }
 
-private func combineEncrypText(isChinese: Bool) -> String {
-    let encrypFilePaths = sortContentOfDirectory(dirPath: outputDirPath)
-    var text = ""
+private func combineEncrypText() throws {
+    let encrypFilePaths = sortContentOfDirectory()
     for path in encrypFilePaths {
-        let s = try! String(
-            contentsOf: URL(fileURLWithPath: path),
-            encoding: isChinese ? .utf8 : .ascii
-        )
-        text += s
+        if FileManager.default.fileExists(atPath: path) {
+            let fileUrl = URL(fileURLWithPath: path)
+            if let fileHandle = try? FileHandle(forWritingTo: fileUrl) {
+                let fileText = try! String(contentsOf: fileUrl, encoding: cnToEn ? .utf8 : .ascii)
+                defer {
+                    fileHandle.closeFile()
+                }
+                fileHandle.seekToEndOfFile()
+                fileHandle.write(fileText.data(using: .utf8)!)
+            } else {
+                fatalError("Cannot get the FileHandle: \(path)")
+            }
+        } else {
+            throw CombineGPGEncrypTextError.invalidFileUrl(path: path)
+        }
     }
-    return text
 }
 
-private func compareText(sourceFilePath: String, isChinese: Bool = false) -> Bool {
-    // 提取原始文本并去除换行符
-    let sourceFileText = try! String(contentsOf: URL(fileURLWithPath: sourceFilePath), encoding: .ascii).filter { $0 != "\n" }
-    // 把切割开来的加密文本重新组合为 encrypText，然后去除换行符，再与 sourceFileText 进行对比
-    var encrypText = ""
-    if isChinese {
-        // 中文密文转换为PGP密文
-        encrypText = String(
-            combineEncrypText(isChinese: true)
-                .filter { $0 != "\n" }
-                .map { alaphabetConvertor($0, ConvertMode.chineseToEnglish) }
-        )
-    } else {
-        encrypText = combineEncrypText(isChinese: false).filter { $0 != "\n" }
-    }
-    return sourceFileText == encrypText
-}
+//private func compareText(sourceFilePath: String, isChinese: Bool = false) -> Bool {
+//    // 提取原始文本并去除换行符
+//    let sourceFileText = try! String(contentsOf: URL(fileURLWithPath: sourceFilePath), encoding: .ascii).filter { $0 != "\n" }
+//    // 把切割开来的加密文本重新组合为 encrypText，然后去除换行符，再与 sourceFileText 进行对比
+//    var encrypText = ""
+//    if isChinese {
+//        // 中文密文转换为PGP密文
+//        encrypText = String(
+//            try! combineEncrypText()
+//                .filter { $0 != "\n" }
+//                .map { alaphabetConvertor($0, ConvertMode.chineseToEnglish) }
+//        )
+//    } else {
+//        encrypText = try! combineEncrypText().filter { $0 != "\n" }
+//    }
+//    return sourceFileText == encrypText
+//}
 
 private func printLog(_ text: String) {
     if let log = printLogSwitch {
@@ -87,20 +93,20 @@ private func readDirectoryPath(path: String) throws -> String {
     }
 }
 
-func main() throws {
+func main(arguments: [String]) throws {
     switch arguments.count {
     case 1:
-        throw CombineGPGEncrypTextError.missingArguments(argumentNames: ["read_directory_path", "write_dir_path"])
+        throw CombineGPGEncrypTextError.missingArguments(argumentNames: ["read_directory_path", "write_file_path"])
     case 2:
-        throw CombineGPGEncrypTextError.missingArgument(argumentName: "write_dir_path")
+        throw CombineGPGEncrypTextError.missingArgument(argumentName: "write_file_path")
     case 3:
-        readDirectoryPath = try readDirectoryPath(path: arguments[1])
+        readDirPath = try readDirectoryPath(path: arguments[1])
         writeFilePath = arguments[2]
     case 4:
-        readDirectoryPath = try readDirectoryPath(path: arguments[1])
+        readDirPath = try readDirectoryPath(path: arguments[1])
         writeFilePath = arguments[2]
     case 5:
-        readDirectoryPath = try readDirectoryPath(path: arguments[1])
+        readDirPath = try readDirectoryPath(path: arguments[1])
         writeFilePath = arguments[2]
         if arguments[3] == "printlog" {
             printLogSwitch = arguments[3]
@@ -108,7 +114,7 @@ func main() throws {
             throw CombineGPGEncrypTextError.invalidArguments(argumentNames: [String](arguments[1..<arguments.count]))
         }
     case 6:
-        readDirectoryPath = try readDirectoryPath(path: arguments[1])
+        readDirPath = try readDirectoryPath(path: arguments[1])
         writeFilePath = arguments[2]
         if arguments[3] == "printlog" {
             printLogSwitch = arguments[3]
@@ -123,6 +129,10 @@ func main() throws {
     default:
         throw CombineGPGEncrypTextError.invalidArguments(argumentNames: [String](arguments[7..<arguments.count]))
     }
+    
+    try combineEncrypText()
 }
 
-try main()
+print("开始执行")
+try main(arguments: arguments)
+print("结束")
